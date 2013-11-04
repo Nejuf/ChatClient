dafc05160d242998ef538e26ff80b251c01827f0
App Academy Student <appacademy@Apps-Mac-mini-69.local>
2013-11-04 11:59:11 -0800

    Adds socket io

--- router.js
+++ router.js
@@ -1,9 +1,19 @@
+var fs = require('fs');
+
 module.exports = function(req, res) {
-  if(req.url == "/"){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+  var url = req.url
+  if(url == "/"){
+    url = "/public/index.html";
   }
-  else {
-    //Load file
-  }
+
+  fs.readFile("."+url, { encoding: "utf8" }, function(error, data) {
+    if(error) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end("File not found.");
+    }
+    else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    }
+  });
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var router = require('./router.js');
+var chat_server = require('./lib/chat_server.js')
 
-http.createServer(router).listen(8080);
+var server = http.createServer(router).listen(8080);
+chat_server.createChat(server);
 
 console.log('Server running at http://127.0.0.1:8080/');
